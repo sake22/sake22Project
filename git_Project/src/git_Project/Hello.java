2e99f755c8c9f6ccbe7024a9b67591fc70cabb20
user <sucm1234@gmail.com>
2019-11-05 10:14:25 +0900

    Hello git

--- git_Project/src/git_Project/Hello.java
+++ git_Project/src/git_Project/Hello.java
@@ -3,6 +3,6 @@ package git_Project;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Java");
+		System.out.println("Git");
 	}
 }
